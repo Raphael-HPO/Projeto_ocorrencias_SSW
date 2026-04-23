56093f18fa76f323fb46d1e2e7cf2703f9b537ce
Orati <r.orati@ativalog.com.br>
2026-04-23 15:39:19 -0300

    refactor: retirada de código para refatoração de projeto em JAVA e SPRINGBOOT.

--- Projeto_ocorrencias_NESTjs/prisma/migrations/20260304183657_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "user" (
-    "id" SERIAL NOT NULL,
-    "key" VARCHAR(100) NOT NULL,
-    "retorno" VARCHAR(100) NOT NULL,
-
-    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
-);
--- Projeto_ocorrencias_NESTjs/prisma/migrations/20260305174054_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[key]` on the table `ocorrencia` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "ocorrencia_key_key" ON "ocorrencia"("key");
